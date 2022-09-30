90422be03601efa279632202426386dbe13ba22e
ykgithuber <94498360+ykgithuber@users.noreply.github.com>
2022-09-30 08:58:18 -0400

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -4,6 +4,10 @@ def call(String stageName){
      {
        sh "mvn clean package"
      }
+  else if ("${stageName}" == "SonarQube Report")
+     {
+       sh "mvn clean sonar:sonar"
+     }
   else if ("${stageName}" == "Upload Into Nexus")
      {
        sh "mvn clean deploy"
